fc52b586057e51c72af492214adfd9c0371def57
Ian Neal Higginson <iannhigginson@hotmail.com>
2022-01-10 08:33:34 +0200

    version 1.1.100122.0900

--- js/msgBox.js
+++ js/msgBox.js
@@ -1,16 +1,21 @@
 /**
- * An updated version of inhMsgBox
- * 23/09/2020 11:00
- * Ian Neal Higginson.
+ * @package msgBox.js
+ * @version 1.1.100122.0900
+ * @date 10/01/2022 09:00
+ * @author Ian Neal Higginson.
  */
 
 function showMsgBox(hed, msg) {
+ /**
+  *# The message box
+  */
  var msgBox = document.createElement("div");
  msgBox.id = "msgBox";
  msgBox.style.backgroundColor = "#575757";
  msgBox.style.color = "white";
  msgBox.style.border = "1px solid black";
  msgBox.style.borderRadius = "5px";
+ msgBox.style.boxShadow = "2px 2px 2px 1px #272727";
  msgBox.style.padding = "10px";
  msgBox.style.marginBottom = "auto";
  msgBox.style.marginLeft = "auto";
@@ -18,38 +23,71 @@ function showMsgBox(hed, msg) {
  msgBox.style.marginTop = "auto";
  msgBox.style.position = "fixed";
  msgBox.style.top = "10px";
- msgBox.style.left = "25%";
- var removemsgBox = function () {
+ msgBox.style.left = "5%";
+ msgBox.style.width = "90%";
+ msgBox.style.height = "90%";
+
+ /**
+  *# Remove message box method
+  */
+ var removemsgBox = () => {
   var existingmsgBox = document.getElementById(msgBox.id);
   if (existingmsgBox && existingmsgBox.parentElement) {
    existingmsgBox.parentElement.removeChild(existingmsgBox);
   }
  };
  removemsgBox();
+
+ /**
+  *# Remove message box button
+  */
  var closeButton = document.createElement("a");
- msgBox.appendChild(closeButton);
  closeButton.textContent = "X";
- closeButton.style.backgroundColor = "#000000";
+ closeButton.style.backgroundColor = "#ff0000";
  closeButton.style.border = "#ffffff solid thin";
  closeButton.style.borderRadius = "5px";
  closeButton.style.color = "#ffffff";
  closeButton.style.cursor = "pointer";
  closeButton.style.float = "right";
  closeButton.style.fontWeight = "bold";
- closeButton.style.padding = "3px";
- closeButton.style.position = "reletive";
- closeButton.style.top = "3px";
- closeButton.onclick = function () {
+ closeButton.style.padding = "10px";
+ closeButton.style.position = "relative";
+ closeButton.style.top = "1px";
+ closeButton.style.zIndex = "1000";
+ closeButton.onclick = () => {
   removemsgBox();
  };
+
+ /**
+  *# Message box header
+  */
  var msgBoxHeader = document.createElement("header");
- msgBox.appendChild(msgBoxHeader);
  msgBoxHeader.innerHTML = hed;
  msgBoxHeader.style.marginLeft = "auto";
  msgBoxHeader.style.marginRight = "auto";
  msgBoxHeader.style.textAlign = "center";
+
+ /**
+  *# The message container
+  */
  var message = document.createElement("section");
+ message.innerHTML = "<hr /><br />" + msg;
+
+ /**
+  *# Put it all togeather
+  *! In this order please!
+  */
+ msgBox.appendChild(closeButton);
+ msgBox.appendChild(msgBoxHeader);
  msgBox.appendChild(message);
- message.innerHTML = '<hr /><br />' + msg;
+
+ /**
+  *# Stick it to the document
+  */
  document.body.appendChild(msgBox);
+
+ /**
+  *# Focus on the message box
+  */
+ msgBox.focus();
 }
